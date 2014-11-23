03353641bd2cfde2ff403eb68b7837f714c08a23
ryanss <ryanssdev@icloud.com>
2014-11-22 23:59:00 -0500

    Add Bootstrap framework to web interface

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 	watchDirs(config.TVShows, ProcessTVShow)
 	auth := httpauth.SimpleBasicAuth(config.Username, config.Password)
 	http.Handle("/", auth(http.HandlerFunc(home)))
-	http.Handle("/rss/movies", auth(http.HandlerFunc(MoviesRSS)))
+	http.Handle("/feed/movies", auth(http.HandlerFunc(MoviesRSS)))
 	movieFileServer := http.FileServer(http.Dir(config.Movies))
 	http.Handle("/media/movies/", auth(http.StripPrefix("/media/movies/", movieFileServer)))
 	http.ListenAndServe(fmt.Sprintf(":%d", config.Port), nil)
