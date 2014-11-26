61db4e5043fcb5bf67d07178534d569e4fb2a1f6
ryanss <ryanssdev@icloud.com>
2014-11-25 23:59:00 -0500

    Add direct links to media files on home page

--- main.go
+++ main.go
@@ -12,11 +12,23 @@ func main() {
 	watchDirs(config.TVShows, ProcessTVShow)
 	watchDir(config.Audio, ProcessAudio)
 	watchDir(config.Video, ProcessVideo)
+
 	auth := httpauth.SimpleBasicAuth(config.Username, config.Password)
 	http.Handle("/", auth(http.HandlerFunc(home)))
 	http.Handle("/feed/movies", auth(http.HandlerFunc(MoviesRSS)))
+
 	movieFileServer := http.FileServer(http.Dir(config.Movies))
 	http.Handle("/media/movies/", auth(http.StripPrefix("/media/movies/", movieFileServer)))
+
+	tvshowFileServer := http.FileServer(http.Dir(config.TVShows))
+	http.Handle("/media/tvshows/", auth(http.StripPrefix("/media/tvshows/", tvshowFileServer)))
+
+	audioFileServer := http.FileServer(http.Dir(config.Audio))
+	http.Handle("/media/audio/", auth(http.StripPrefix("/media/audio/", audioFileServer)))
+
+	videoFileServer := http.FileServer(http.Dir(config.Video))
+	http.Handle("/media/video/", auth(http.StripPrefix("/media/video/", videoFileServer)))
+
 	http.ListenAndServe(fmt.Sprintf(":%d", config.Port), nil)
 }
 
